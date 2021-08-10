76c8b90dacb08c4c77a04cc69a92063c3fa6a7b7
SirJohnSem <jewgienijdanilczik@mail.ru>
2021-08-10 14:03:43 +0300

    add contact page

--- src/contact.js
+++ src/contact.js
@@ -3,7 +3,8 @@ export function getContactPage() {
     content.innerHTML = '';
 
     const contact = document.createElement('div');
-    contact.innerHTML = 'Contact information';
+    contact.innerHTML = '<h3>CONTACT DETAILS</h3>'+'<h2>Restaurant Prime Cut</h2>'+'<p>Twarda 18, 00-105 Warsaw<p>'+'<p>tel: +48 733 08 22 33<p>'+'<p>e-mail: reservation@primecut.pl<p>'+'<h2>OPENING HOURS</h2>'+'<p>Monday - Thursday: 12 a.m. - 11 p.m.<p>'+'<p>Friday: 12 a.m. - 12 p.m.<p>'+'<p>Saturday: 4 p.m. - 12 p.m.<p>'+'<p>Sunday: 4 p.m. - 10 p.m.<p>';
+    contact.classList.add('contact-page');
 
     return content.appendChild(contact);
 }
